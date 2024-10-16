b1bb8e50723b938d84b6a597e7dff3bc2f620ffb
aswinning <aswinrox03@gmail.com>
2024-10-16 20:47:03 +0530

    first commit

---
+++ lib/pages/counterpage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("you've clicked this button this many times!"),
+            Text(
+              _counter.toString(),
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            ElevatedButton(onPressed: _incrementCounter, child: Text("Increment!")),
+          ],
+        ),
+      ),
+    );
+  }
+}
